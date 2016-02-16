d77e2998f26d57b75791be4e782bc032cef09395
Bogdan Sliptsov <boubdyk@mail.ru>
2016-02-16 23:06:54 +0200

    first try of Fourier series

---
+++ src/main/java/com/diploma/approximation/FourierSeries.java
@@ -0,0 +1,74 @@
+package com.diploma.approximation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by boubdyk on 16.02.2016.
+ */
+public class FourierSeries {
+
+    private List<Double> yValues;
+    private List<Double> xValues;
+    private List<Point> points;
+    private final int N;
+
+    public FourierSeries(List<Point> points) {
+        List<Double> yValues = new ArrayList<Double>();
+        List<Double> xValues = new ArrayList<Double>();
+        for (Point p: points) {
+            yValues.add(p.getY());
+            xValues.add(p.getX());
+        }
+        this.points = points;
+        this.yValues = yValues;
+        this.xValues = xValues;
+        this.N = points.size();
+           }
+
+    private double countSum() {
+        double sum = 0.0;
+        for (Double d: yValues) {
+            sum += d;
+        }
+        return sum;
+    }
+
+    private Double countA0() {
+        return 1.0 / yValues.size() * countSum();
+    }
+
+    private Double countAk(int k, Double t) {
+        return 2.0 / N * countSum() * Math.cos(k * t);
+    }
+
+    private Double countBk(int k, Double t) {
+        return 2.0 / N * countSum() * Math.sin(k * t);
+    }
+
+
+    // 4 in loop is max number of garmonics
+    public Double calculation(Double t) {
+        List<Double> sumResults = new ArrayList<Double>();
+        List<Double> differenceList = new ArrayList<Double>();
+        for (int i = 0; i < 10; i++) {
+            double tmpResult = 0.0;
+            for (int j = 1; j <= i + 1; j++) {
+                tmpResult += countAk(j, t) * Math.cos(j * t) + countBk(j, t) * Math.sin(j * t);
+            }
+            sumResults.add(countA0() + tmpResult);
+            differenceList.add(Math.sqrt(Math.pow(sumResults.get(i) - t, 2)));
+        }
+        return sumResults.get(differenceList.indexOf(Collections.min(differenceList)));
+    }
+
+    public List<Double> fourierSeriesApproximation() {
+        List<Double> resultList = new ArrayList<Double>();
+        for (int i = 0; i < N; i++) {
+            resultList.add(calculation(yValues.get(i)));
+        }
+        return resultList;
+    }
+
+}
--- src/main/java/com/diploma/approximation/Test.java
+++ src/main/java/com/diploma/approximation/Test.java
@@ -13,18 +13,18 @@ public class Test {
     public static void testPolynomialApproximation(List<Point> points) {
         PolynomialApproximation testCase = new PolynomialApproximation(points);
 
-        System.out.println("\n\n" + testCase.approximateFunction(0.0, 6) + "\n");
-
-        for (Double d: testCase.standardDeviation(10)) {
-            System.out.println(d);
-        }
+//        System.out.println("\n\n" + testCase.approximateFunction(0.0, 6) + "\n");
+//
+//        for (Double d: testCase.standardDeviation(10)) {
+//            System.out.println(d);
+//        }
 
         //The most accuracy function
-        System.out.println("\nFunction=" + testCase.solveLinearEquation(testCase.bestExponent(testCase.standardDeviation(10))));
+//        System.out.println("\nFunction=" + testCase.solveLinearEquation(testCase.bestExponent(testCase.standardDeviation(10))));
 
-        System.out.println("\n\n" + testCase.approximateFunction(3.0, testCase.bestExponent(testCase.standardDeviation(10))));
+        System.out.println("\n\n" + testCase.approximateFunction(4.0, 3));
 
-        System.out.println(testCase.solveLinearEquation(6));
+//        System.out.println(testCase.solveLinearEquation(6));
     }
 
     public static void testEponentialSmoothing(List<Point> points) {
@@ -39,6 +39,12 @@ public class Test {
         }
     }
 
+    public static void testFourierSeries(List<Point> points) {
+        for (Double d: new FourierSeries(points).fourierSeriesApproximation()) {
+            System.out.println(d);
+        }
+    }
+
 
     public static void main(String[] args) {
         List<Point> points = new ArrayList<Point>();
@@ -48,8 +54,9 @@ public class Test {
         points.add(new Point(1.0, 2.3));
         points.add(new Point(3.0, 1.5));
 
-        testPolynomialApproximation(points);
+//        testPolynomialApproximation(points);
 //        testEponentialSmoothing(points);
+        testFourierSeries(points);
 
     }
 
